123886cc19142dbab5fb75822d7aec203663619f
Michael Babiy <ic.babiy@gmail.com>
2015-01-25 21:45:18 -0800

    Initial Commit

---
+++ RMParallax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RMParallax
+//
+//  Created by Michael Babiy on 1/25/15.
+//  Copyright (c) 2015 Raphael Miller & Michael Babiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
